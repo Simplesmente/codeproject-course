86f473eae6fe69a82df969107eb3d050ef94686d
André Teles <andre@macacoLouco>
2016-03-29 16:22:52 -0300

    fixed problema with generation of seeds, next leason 'Testando Relacionamento'

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,8 +12,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        DB::statement("SET foreign_key_checks = 0");
 
         $this->call(ClientTableSeeder::class);
         $this->call(ProjectTableSeeder::class);
+
+        DB::statement("SET foreign_key_checks = 1");
     }
 }
