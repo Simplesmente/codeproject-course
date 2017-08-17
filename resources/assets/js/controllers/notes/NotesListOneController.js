3618b1693f6809410b8b7980049a5a6d8dc2c7bf
André Teles <winchestersystem@gmail.com>
2017-08-17 20:33:07 -0300

    Adicionando de listar apenas uma nota

--- resources/assets/js/controllers/notes/NotesListOneController.js
+++ resources/assets/js/controllers/notes/NotesListOneController.js
@@ -1,13 +1,17 @@
 angular.module('app.controllers')
         .controller('NotesListOneController',['$scope','$routeParams','Notes',function($scope,$routeParams,Notes){
           
-          console.log('aquiiiiii');
-          //console.log($routeParams);
+          console.log($routeParams);
           
           var params = {
                         idProject:$routeParams.idProject,
                         id:$routeParams.id
                       };
+
+          var result = Notes.query(params,function(data){
+                 $scope.note = data;
+                 console.log(data);
+          });
 
 
 
